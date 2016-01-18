3659effbedc5351a50ac061948d9d794163cdfe1
Jeff Lyon <jeff@albatrossdigital.com>
2016-01-17 18:07:27 -0800

    Re-adding Alexs changes to auto WR template builder, fixing issues

--- wp-proud-agency.php
+++ wp-proud-agency.php
@@ -8,9 +8,7 @@ Author: ProudCity
 Author URI: http://proudcity.com/
 License: GPLv2
 */
-
 // @todo: use CMB2: https://github.com/WebDevStudios/CMB2 or https://github.com/humanmade/Custom-Meta-Boxes
-
 namespace Proud\Agency;
 
 // Load Extendible
@@ -20,14 +18,11 @@ if ( ! class_exists( 'ProudPlugin' ) ) {
 }
 
 class Agency extends \ProudPlugin {
-
-
   public function __construct() {
     parent::__construct( array(
       'textdomain'     => 'wp-proud-agency',
       'plugin_path'    => __FILE__,
     ) );
-
     $this->hook( 'init', 'create_agency' );
     $this->hook( 'admin_init', 'agency_admin' );
     $this->hook( 'plugins_loaded', 'agency_init_widgets' );
@@ -36,10 +31,10 @@ class Agency extends \ProudPlugin {
     $this->hook( 'save_post', 'add_agency_contact_fields', 10, 2 );
     $this->hook( 'rest_api_init', 'agency_rest_support' );
     $this->hook( 'before_delete_post', 'delete_agency_menu' );
-
+    // @todo fix this???
+    // add_action( 'save_post', 'my_project_updated_send_email' );
     add_filter( 'template_include', array($this, 'agency_template') );
     add_filter( 'wp_insert_post_data' , array($this, 'add_agency_wr_code') , -10, 2 );
-
   }
 
   // Init on plugins loaded
@@ -183,7 +178,6 @@ class Agency extends \ProudPlugin {
     );
   }
 
-
   public function display_agency_social_meta_box( $agency ) {
     foreach ($this->agency_social_services() as $service => $label) {
       $value = esc_html( get_post_meta( $agency->ID, 'social_'.$service, true ) );
@@ -226,9 +220,7 @@ class Agency extends \ProudPlugin {
     //  update_post_meta( $agency->ID, '_wr_deactivate_pb', 0 );
     //}
     //update_post_meta( $agency->ID, '_wr_page_active_tab', 1 );
-
     $menus = get_registered_nav_menus();
-
     $menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
     global $menuArray;
     $menuArray = array(
@@ -238,12 +230,13 @@ class Agency extends \ProudPlugin {
     foreach ( $menus as $menu ) {
       $menuArray[$menu->slug] = $menu->name;
     }
+
     $type = get_post_meta( $agency->ID, 'agency_type', true );
     //$type = $type ? $type : 'page';
     $menu = get_post_meta( $agency->ID, 'post_menu', true );
     $menu = $menu ? $menu : 'new';
-
     $isNew = empty($agency->post_title) ? 1 : 0;
+
     ?>
       <label style="margin-top: .5em;">Agency type:</label>
       <div class="checkboxes">
@@ -345,7 +338,6 @@ class Agency extends \ProudPlugin {
           update_post_meta( $id, $field, $_POST['agency_'.$field] );
         //}
       }
-
     }
   }
 
@@ -353,10 +345,14 @@ class Agency extends \ProudPlugin {
    * Adds the Woo Rockets code if none is set and this is a section.
    */
   public function add_agency_wr_code( $data , $postarr ) {
-    if ('section' === $_POST['agency_type'] && empty($data['post_content'])) {
-      $data['post_content'] = $this->agency_wr_code($data['post_title'], get_the_post_thumbnail_url($data['ID']));
-      update_post_meta( $data['ID'], '_wr_page_builder_content', $data['post_content'] );
-      update_post_meta( $data['ID'], '_wr_page_active_tab', 1 );
+    // Add default styles + content ?
+    if ('section' === $postarr['agency_type'] && !empty($postarr['ID']) && empty($data['post_content'])) {
+      // Hide page title + make full width
+      $_POST['proud_full_width'] = 'on';
+      $_POST['proud_hide_title'] = 'on';
+      // Let WR handle default content.
+      $_POST['shortcode_content'] = $this->agency_wr_code($data['post_title'], get_the_post_thumbnail_url($postarr['ID']));
+      $_POST['wr_active_tab'] = 1;
     }
     return $data;
   }
@@ -373,7 +369,24 @@ class Agency extends \ProudPlugin {
    * Returns the default Woo Rockets code for agencies
    */
   private function agency_wr_code($title, $image) {
-    return '[wr_row width="full" background="none" border_width_value_="0" border_style="solid" border_color="#000" div_padding_top="10" div_padding_left="10" div_padding_bottom="10" div_padding_right="10" ][wr_column span="span12" ][wr_jumbotronheader div_margin_top="0" div_margin_left="0" div_margin_bottom="25" div_margin_right="0" include_title="no" background="image" image="'.$image.'" img_repeat="none" background_size="normal" paralax="no" make_inverse="no" box_background="none" disabled_el="no" ]<h1>'.$title.'</h1>[/wr_jumbotronheader][/wr_column][/wr_row][wr_row width="boxed" background="none" solid_color_value="#FFFFFF" solid_color_color="#ffffff" gradient_color="0% #FFFFFF,100% #000000" gradient_direction="vertical" repeat="full" img_repeat="full" autoplay="yes" position="center center" paralax="no" border_width_value_="0" border_style="solid" border_color="#000" div_padding_top="10" div_padding_bottom="10" div_padding_right="10" div_padding_left="10" ][wr_column span="span4" ][wr_widget widget_id="AgencyMenu"]widget-agency_menu%5B%5D%5Btitle%5D=[/wr_widget][wr_widget widget_id="AgencyContact"]widget-agency_contact%5B%5D%5Btitle%5D=Contact[/wr_widget][wr_widget widget_id="AgencySocial"]widget-agency_social%5B%5D%5Btitle%5D=Connect[/wr_widget][wr_widget widget_id="AgencyHours"]widget-agency_hours%5B%5D%5Btitle%5D=Hours[/wr_widget][/wr_column][wr_column span="span8" ][wr_text text_margin_top="0" text_margin_bottom="0" enable_dropcap="no" appearing_animation="0" disabled_el="no" ][/wr_text][/wr_column][/wr_row]';
+    return [
+      '[wr_row width="full" background="none" border_width_value_="0" border_style="solid" border_color="#000" div_padding_top="10" div_padding_left="10" div_padding_bottom="10" div_padding_right="10" ]',
+      '[wr_column span="span12" ]',
+      '[wr_jumbotronheader div_margin_top="0" div_margin_left="0" div_margin_bottom="25" div_margin_right="0" include_title="no" background="image" image="'.$image.'" img_repeat="none" background_size="normal" paralax="no" make_inverse="no" box_background="none" disabled_el="no" ]<h1>'.$title.'</h1>[/wr_jumbotronheader]',
+      '[/wr_column]',
+      '[/wr_row]',
+      '[wr_row width="boxed" background="none" solid_color_value="#FFFFFF" solid_color_color="#ffffff" gradient_color="0% #FFFFFF,100% #000000" gradient_direction="vertical" repeat="full" img_repeat="full" autoplay="yes" position="center center" paralax="no" border_width_value_="0" border_style="solid" border_color="#000" div_padding_top="10" div_padding_bottom="10" div_padding_right="10" div_padding_left="10" ]',
+      '[wr_column span="span4" ]',
+      '[wr_widget widget_id="AgencyMenu"]widget-agency_menu%5B%5D%5Btitle%5D=[/wr_widget]',
+      '[wr_widget widget_id="AgencyContact"]widget-agency_contact%5B%5D%5Btitle%5D=Contact[/wr_widget]',
+      '[wr_widget widget_id="AgencySocial"]widget-agency_social%5B%5D%5Btitle%5D=Connect[/wr_widget]',
+      '[wr_widget widget_id="AgencyHours"]widget-agency_hours%5B%5D%5Btitle%5D=Hours[/wr_widget]',
+      '[/wr_column]',
+      '[wr_column span="span8" ]',
+      '[wr_text text_margin_top="0" text_margin_bottom="0" enable_dropcap="no" appearing_animation="0" disabled_el="no" ][/wr_text]',
+      '[/wr_column]',
+      '[/wr_row]'
+    ];
   }
 
 } // class
@@ -392,4 +405,18 @@ function get_agency_permalink($post = 0) {
   else {
     return esc_url( apply_filters( 'the_permalink', get_permalink( $post ), $post ) );
   }
+}
+
+/**
+ * Returns the list of social fields (also sued in agency-social-links-widget.php)
+ */
+function agency_social_services() {
+  return array(
+    'facebook' => 'http://facebook.com/pages/',
+    'twitter' => 'http://twitter.com/',
+    'instagram' => 'http://instagram.com/',
+    'youtube' => 'http://youtube.com/',
+    'rss' => 'Enter url to RSS news feed',
+    'ical' => 'Enter url to iCal calendar feed',
+  );
 }
