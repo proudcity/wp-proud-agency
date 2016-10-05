b4c192f066c476eb9e2d6d1a8a93630c99e4025e
Jeff Lyon <jeff@albatrossdigital.com>
2016-10-05 16:41:11 -0700

    Adding contact name link field https://github.com/proudcity/wp-proudcity/issues/593

--- widgets/agency-contact-widget.class.php
+++ widgets/agency-contact-widget.class.php
@@ -27,6 +27,7 @@ class AgencyContact extends Core\ProudWidget {
     global $pageInfo;
     $id = get_post_type() === 'agency' ? get_the_ID(): $pageInfo['parent_post'];
     $instance['name'] = get_post_meta( $id, 'name', true );
+    $instance['name_link'] = get_post_meta( $id, 'name_link', true );
     $instance['email'] = get_post_meta( $id, 'email', true );
     $instance['phone'] = get_post_meta( $id, 'phone', true );
     $instance['fax'] = get_post_meta( $id, 'fax', true );
@@ -39,14 +40,20 @@ class AgencyContact extends Core\ProudWidget {
 
   /**
    * Outputs the content of the widget
-   *
+   *https://github.com/proudcity/wp-proudcity/issues/545
    * @param array $args
    * @param array $instance
    */
   public function printWidget( $args, $instance ) {
     extract( $instance );
     ?>
-    <?php if($name): ?><p class="field-contact-name"><?php print esc_html($name) ?></p><?php endif; ?>
+    <?php if($name): ?><p class="field-contact-name">
+      <?php if( !empty($name_link) ): ?>
+        <?php print sprintf( '<a href="%s" rel="bookmark">%s</a>', esc_url( $name_link ), esc_html($name) ); ?>
+      <?php else: ?>
+        <?php print esc_html($name) ?>
+      <?php endif; ?>
+    </p><?php endif; ?>
     <?php if($phone || $email): ?><p>
       <?php if($phone): ?><div class="field-contact-phone"><a href="tel:<?php print esc_url($phone) ?>"><i class="fa fa-fw fa-phone"></i><?php print esc_html($phone) ?></a></div><?php endif; ?>
       <?php if($fax): ?><div class="field-contact-fax"><a href="tel:<?php print esc_url($fax) ?>"><i class="fa fa-fw fa-fax"></i><?php print esc_html($fax) ?></a></div><?php endif; ?>
